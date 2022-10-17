230a84fccd811c6823b69d026b8b780b135b9f9a
Benjamin Solberg <benjiwow99@gmail.com>
2022-10-17 22:10:20 +0000

    add import of random for computer and randomize game and introduction funtion with an start game funtion

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+def introduction():
+    print("--------------------------------------------------------------------------------------")
+    print("                           Hello and welcome to Battleshipx!")
+    print("--------------------------------------------------------------------------------------")
+    print(" This is my first attempt in making this type of game hopes it works and is enjoyable.")
+    print("--------------------------------------------------------------------------------------")
+    print("")
+    print("")
+    print("                            Press ENTER to start the game")
+    print(input(""))
+
+
+
+def start_game():
+    introduction()
+
+
+start_game()        
